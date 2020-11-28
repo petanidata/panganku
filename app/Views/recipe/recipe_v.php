4314203430ff975fbc8728164531ac816f123aa2
j3c118080 <72536297+j3c118080@users.noreply.github.com>
2020-11-28 14:32:40 +0700

    edit1

--- app/Views/recipe/recipe_v.php
+++ app/Views/recipe/recipe_v.php
@@ -19,7 +19,7 @@
             <input type="text" placeholder="cari resep">
         </div>
     </div>
-    <div class="container">$routes->get('/recipe/create','Recipe::create');
+    <div class="container">
         <div class="section-title text-right">
             <a href="<?= base_url('recipe/create');?>" class="btn btn-success"> Tambah Resep </a>
         </div>
