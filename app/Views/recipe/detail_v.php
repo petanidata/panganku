bb452089cbd64c65003d375578d71012d4cc8620
j3c118080 <72536297+j3c118080@users.noreply.github.com>
2020-11-30 16:32:48 +0700

    update detail

--- app/Views/recipe/detail_v.php
+++ app/Views/recipe/detail_v.php
@@ -7,66 +7,60 @@
 
   <section id="features" class="padd-section text-center wow fadeInUp">
   <div class="container">
-        <div class="section-title text-center">
-            <?php if(session()->getFlashData('pesan')) :?>
-              <div class="alert alert-success" role="alert">
-                <?= session()->getFlashData('pesan') ?>
-              </div>
-            <?php endif;?>
-        </div>
+    <div class="section-title text-center">
+      <?php if(session()->getFlashData('pesan')) :?>
+          <div class="alert alert-success" role="alert">
+            <?= session()->getFlashData('pesan') ?>
+          </div>
+        <?php endif;?>
     </div>
-    <div class="container">
+  </div>
+  <div class="container">
     <div class="d-flex">
-        <a href="/recipe" class="btn btn-success"> Kembali ke resep</a>
-        <?php if((session()->get('id_user')==$resep['id_user']) or (session()->get('is_admin')=="Y" )) { ?>
+      <a href="/recipe" class="btn btn-success"> Kembali ke resep</a>
+      <?php if((session()->get('id_user')==$resep['id_user']) or (session()->get('is_admin')=="Y" )) { ?>
         <a href="/recipe/edit/<?=$resep['id_resep'];?>" class="btn btn-warning ml-auto mr-2"> Edit Resep</a>
         <form action="/recipe/delete/<?=$resep['id_resep'];?>" method="post">
           <?= csrf_field()?>
-          <input type="hidden" name="_method" value="DELETE">
-          <button type="submit" class="btn btn-danger" > Hapus Resep </button>
+            <input type="hidden" name="_method" value="DELETE">
+            <button type="submit" class="btn btn-danger" > Hapus Resep </button>
         </form>
-        <?php }?>
-
-        
-        
+      <?php }?>
+    </div>
+    <div class="card d-flex" style="border: none">
+      <h2><b><?= $resep['judul'];?></b></h2>
+      <p class="separator"> oleh </p><b><?= $resep['nama_lengkap'];?></b>
+      <div>
+        <img src="<?php echo base_url('/img/recipe/'.$resep['gambar_banner']); ?>" alt="artikel1" width="75%">
       </div>
-      <div class="section-title text-center">
-        <h2><b><?= $resep['judul'];?></b></h2>
-        <p class="separator"> oleh </p><b><?= $resep['nama_lengkap'];?></b>
+      <br /><br />
+      <H4> Lama memasak </h4>  
+      <h5><?php echo $resep['lama_memasak'];?> Menit</h5>
+      <br />
+      <H4> Jumlah porsi </h4>
+      <h5><?php echo $resep['porsi'];?></h5>
+      <br />
+      <H4> Bahan - Bahan </h4>
+      <div class="justify">
+        <h5 ><?= nl2br($resep['bahan']);?></h5>
       </div>
-      <div class="row text-center align-self-center">
-        <div class="col-md-12 col-lg-12">
-          <div class="feature-block w-100">
-            <img src="<?php echo base_url('/img/recipe/'.$resep['gambar_banner']); ?>" alt="artikel1" width="75%">
-            <br /><br />
-            <H4> Lama memasak </h4>
-            <h5><?php echo $resep['lama_memasak'];?> Menit</h5>
-            <br />
-            <H4> Jumlah porsi </h4>
-            <h5><?php echo $resep['porsi'];?></h5>
-            <br />
-            <H4> Bahan - Bahan </h4>
-            <h5 ><?= nl2br($resep['bahan']);?></h5>
-            <br />
-            <H4> Cara memasak </h4>
-            <h5><?php echo nl2br($resep['tutorial']);?></h5>
-            
-            
-          </div>
-        </div>
-        <div class="col-md-12 col-lg-12 mt-2">
-          <H4> Gambar Tutorial </h4>
-        </div>
-        
-        <?php if ($resep['gambar_tutorial'] != "") {
-           $i=1; $tutorial = explode(",",$resep['gambar_tutorial']);foreach ($tutorial as $t) { ?>
-          <div class="col-md-3 col-lg-3 w-100 my-2">
-          <img src="<?php echo base_url('img/recipe/'.$t); ?>" alt="img<?= $i++; ?>" width="100%" >
-          </div>
-        <?php }} ?> 
-
+      <br/>
+      <H4> Cara memasak </h4>
+      <div class="justify">
+        <h5><?php echo nl2br($resep['tutorial']);?></h5>
+      </div>
+      <H4> Gambar Tutorial </h4>
+      <div class="row text-center center justify-content-md-center">
+          <?php if ($resep['gambar_tutorial'] != "") {
+            $i=1; $tutorial = explode(",",$resep['gambar_tutorial']);
+            foreach ($tutorial as $t) { ?>
+            <div class="col-md-3 col-lg-3 mt-2 ">
+              <img src="<?php echo base_url('img/recipe/'.$t); ?>" alt="img<?= $i++; ?>" width="100%" >
+            </div>
+          <?php }} ?> 
       </div>
     </div>
+  </div>
   </section>
 
 
